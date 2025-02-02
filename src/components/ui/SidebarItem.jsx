e0c56257f0adb255ba85c5d222cb8f2644b72d58
goldic <contact@goldic.xyz>
2025-02-02 19:01:36 +0400

    Make sidebar as chakra-ui Link component

--- src/components/ui/SidebarItem.jsx
+++ src/components/ui/SidebarItem.jsx
@@ -1,32 +1,33 @@
-import { Text } from "@chakra-ui/react";
+import { Link, Text } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 
-const SidebarItem = ({ name, icon, link }) => {
+const SidebarItem = ({ name, icon, link, ...props }) => {
+  // Can't use useState here, forced to use vanilla css
   return (
-    <NavLink
+    <Link
+      as={NavLink}
       to={link}
-      // There is not way to use internal chakra styles with NavLink
-      // So I go dirty and write plain css but with chakra vars
-      // TODO: use useState for styles
+      {...props}
+      display={"flex"}
+      alignItems={"center"}
+      w={"full"}
+      gap={6}
+      padding={"12px 24px"}
+      textDecoration={"none"}
+      transition={"background .1s ease-in"}
       style={({ isActive }) => ({
-        display: "flex",
-        alignItems: "center",
-        width: "100%",
-        gap: "16px",
-        padding: "12px 24px",
-        textDecoration: "none",
-        transition: "background .1s ease-in",
         color: isActive ? "var(--chakra-colors-primary-600)" : "inherit",
         background: isActive
           ? "var(--chakra-colors-primary-100)"
           : "transparent",
+        textDecoration: "none", // For some reason working only here
       })}
     >
       {icon}
       <Text size="lg" color="inherit">
         {name}
       </Text>
-    </NavLink>
+    </Link>
   );
 };
 
